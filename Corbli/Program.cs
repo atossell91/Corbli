da0c75981c926a38e3da4b12e3a62542fe9afc4d
Anthony Tossell <atossell91@gmail.com>
2024-01-27 21:09:56 -0800

    dotnet 8

--- Corbli/Program.cs
+++ Corbli/Program.cs
@@ -1,4 +1,5 @@
 using MySqlConnector;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+
+    app.UseForwardedHeaders(new ForwardedHeadersOptions
+    {
+        ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+    });
+
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
