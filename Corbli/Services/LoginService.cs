7f98f39da71fb3899b956e4b53286e9d7bdb4e53
Anthony Tossell <atossell91@gmail.com>
2024-01-29 21:35:00 -0700

    Added login stubs

---
+++ Corbli/Services/LoginService.cs
@@ -0,0 +1,17 @@
+﻿namespace Corbli.Services
+{
+    public class LoginService
+    {
+        public static Byte[] HashPassword(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool TryLogin(string username, string password)
+        {
+            var passHash = HashPassword(password);
+
+            return false;
+        }
+    }
+}
