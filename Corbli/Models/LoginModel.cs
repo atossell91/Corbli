9e8bc500e35ec5075ce45cc1bf2a4940877c9328
Anthony Tossell <atossell91@gmail.com>
2024-01-29 21:27:28 -0700

    Modified LoginModel

--- Corbli/Models/LoginModel.cs
+++ Corbli/Models/LoginModel.cs
@@ -2,6 +2,12 @@
 {
     public class LoginModel
     {
+        public LoginModel(string username, string password)
+        {
+            UserName = username;
+            Password = password;
+        }
+
         public string UserName { get; set;}
         public string Password { get; set;}
     }
